02ec9a959cea57d84f14697ae1a541d7eeda1fb0
LadyNogitsune <tran.yvonne97@gmail.com>
2021-02-07 18:49:35 -0700

    Update script.js

--- script.js
+++ script.js
@@ -15,23 +15,23 @@ let month = months[now.getMonth()]
 time.innerHTML = "Today's date is "+`${day}, ${month} ${date}, ${years}`
 
 //temperature
-function convertToFarenheit(event){
+function convertToFarenheit(event,){
   event.preventDefault()
   let temperatureElement = document.querySelector(".temperature");
   let temperature = temperatureElement.innerHTML;
-  temperatureElement.innerHTML = Math.round((temperature * 9)/5 + 32)
+  temperatureElement.innerHTML = Math.round((temperature * 1.8)+ 32)
 }
 
 function convertToCelsius (event){
   event.preventDefault()
   let temperatureElement = document.querySelector(".temperature");
   let temperature = temperatureElement.innerHTML;
-  temperatureElement.innerHTML = Math.round((temperature -32) * 9/5)
+  temperatureElement.innerHTML = Math.round((temperature -32) * 0.5556)
 }
 let farenheitLink = document.querySelector(".faren")
 farenheitLink.addEventListener("click", convertToFarenheit)
 
-// MUST FIX LATER
+
 let celsiusLink = document.querySelector(".celsi")
 celsiusLink.addEventListener("click", convertToCelsius)
 
@@ -69,7 +69,7 @@ function showPosition(position) {
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
 
-axios.get(`${apiUrl}lat=${lat}&lon=${lon}&appid=${apiKey}`).then();
+axios.get(`${apiUrl}lat=${lat}&lon=${lon}&appid=${apiKey}`).then(showTemp);
 
 
  // function showTemp (response){
@@ -83,3 +83,27 @@ navigator.geolocation.getCurrentPosition(showPosition)
 
 let current = document.querySelector ('#current')
 current.addEventListener("click",showPosition)
+
+// forcast
+
+function searchWeather(event){
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-text-input");
+
+  console.log(searchInput.value);
+  let h2 = document.querySelector ("h2");
+  h2.innerHTML = searchInput.value;
+
+let city = searchInput.value
+let apiKey = "a0c13833abc217a0181faa947d5ff7c5"
+let apiUrl = "https://api.openweathermap.org/data/2.5/weather?"
+
+function showTemp (response){
+  console.log (response.data.main.temp)
+  let change = document.querySelector(".temperature")
+  change.innerHTML = response.data.main.temp
+}
+axios.get(`${apiUrl}q=${city}&appid=${apiKey}&units=metric`).then (showTemp)
+}
+let apiKey = "a0c13833abc217a0181faa947d5ff7c5";
+let apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=&appid=${apiKey}"
